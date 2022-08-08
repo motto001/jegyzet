e46947cad0590f99182563f842bc3634cde5757b
motto001 <menkuotto@gmail.com>
2022-08-08 19:33:57 +0200

    ggg2

--- debian11_setup.php
+++ debian11_setup.php
@@ -1,13 +1,14 @@
-sudo & user---------------------
+<?php
+//sudo & user rootként---------------------
 apt update &&  apt upgrade -y
-# apt install sudo
+apt install sudo
 adduser motto  (bekéri a jelszót többit leokézni)
 usermod -aG sudo motto
 teszt:
 su motto
 sudo apt update
 
-webszerver ólaravel-----------------------
+//webszerver laravel-----------------------
 https://www.linuxtuto.com/how-to-install-laravel-9-on-debian-11/
 $ 
 sudo apt update && sudo apt upgrade -y
@@ -17,7 +18,7 @@ sudo systemctl start apache2
 Then enable it to start on boot time.
  sudo systemctl enable apache2
 
- php---------------
+ //php---------------
 a 8.1-es nics alapból a debiánban sima telepítéssel csak a 7-est teszi fel
 le kell tölteni:
 sudo apt-get install ca-certificates apt-transport-https software-properties-common -y
@@ -33,7 +34,7 @@ telepítés: a warning ellenére lefut
 sudo apt purge PHP7.4
 sudo apt-get autoremove
 
-maria db--------------------
+//maria db-------------user:laravel_user pass:Password -------
 sudo apt install mariadb-server mariadb-client
 systemctl start mariadb
 systemctl enable mariadb
@@ -45,7 +46,7 @@ MariaDB [(none)]>
         EXIT
 
 
-composer telepítés---------------------
+//composer telepítés---------------------
 curl:
 sudo apt install curl
 curl -sS https://getcomposer.org/installer | sudo php  //sudo-t én írtam be
@@ -53,7 +54,7 @@ sudo mv composer.phar  /usr/local/bin/composer
 sudo chmod +x   /usr/local/bin/composer
 composer --version
 
-laravel telepítés---------------
+//laravel telepítés---------------
 cd /var/www/html
 sudo composer create-project laravel/laravel laravelapp  //sudo-ra a composer tiltakozik! de ha nincs akkor meg permission hiba!
 sudo chown -R www-data:www-data /var/www/html/laravelapp
@@ -84,7 +85,7 @@ CustomLog ${APACHE_LOG_DIR}/your-domain.com_access.log combined
 sudo a2ensite laravel.conf
 sudo systemctl restart apache2
 
-git------------------------------------------
+//git------------------------------------------
 sudo rm -r dirname  //törlés rekurzívan ha már van egy laravel ami nem kell
 sudo apt update
 sudo apt install git
@@ -117,7 +118,7 @@ ssh-keygen -t ed25519 -C "motto001@github.com"  // ssh kulcs létrehozása githu
 eval "$(ssh-agent -s)"   // ssh kulcs aktiválása (nem múködött vele a push)
 
 
-phpmyadmin-------------
+//phpmyadmin------------------------------------------
 
 Download latest version of phpMyAdmin with wget command.
 
@@ -125,3 +126,90 @@ DATA="$(wget https://www.phpmyadmin.net/home_page/version.txt -q -O-)"
 URL="$(echo $DATA | cut -d ' ' -f 3)"
 VERSION="$(echo $DATA | cut -d ' ' -f 1)"
 wget https://files.phpmyadmin.net/phpMyAdmin/${VERSION}/phpMyAdmin-${VERSION}-all-languages.tar.gz
+
+tar xvf phpMyAdmin-${VERSION}-all-languages.tar.gz
+
+sudo mv phpMyAdmin-*/ /usr/share/phpmyadmin
+
+Create directory for phpMyAdmin temp files:
+
+sudo mkdir -p /var/lib/phpmyadmin/tmp
+sudo chown -R www-data:www-data /var/lib/phpmyadmin
+sudo mkdir /etc/phpmyadmin/
+
+sudo cp /usr/share/phpmyadmin/config.sample.inc.php  /usr/share/phpmyadmin/config.inc.php
+
+$ sudo vim /usr/share/phpmyadmin/config.inc.php
+$cfg['blowfish_secret'] = 'H2OxcGXxflSd8JwrwVlh6KW6s2rER63i'; 
+$cfg['TempDir'] = '/var/lib/phpmyadmin/tmp';  //ennélkül is működik
+
+sudo nano /etc/apache2/conf-enabled/phpmyadmin.conf
+
+And paste below contents to the file:
+----------------------------------------------------------------------
+Alias /phpmyadmin /usr/share/phpmyadmin
+
+<Directory /usr/share/phpmyadmin>
+    Options SymLinksIfOwnerMatch
+    DirectoryIndex index.php
+
+    <IfModule mod_php5.c>
+        <IfModule mod_mime.c>
+            AddType application/x-httpd-php .php
+        </IfModule>
+        <FilesMatch ".+\.php$">
+            SetHandler application/x-httpd-php
+        </FilesMatch>
+
+        php_value include_path .
+        php_admin_value upload_tmp_dir /var/lib/phpmyadmin/tmp
+        php_admin_value open_basedir /usr/share/phpmyadmin/:/etc/phpmyadmin/:/var/lib/phpmyadmin/:/usr/share/php/php-gettext/:/usr/share/php/php-php-gettext/:/usr/share/javascript/:/usr/share/php/tcpdf/:/usr/share/doc/phpmyadmin/:/usr/share/php/phpseclib/
+        php_admin_value mbstring.func_overload 0
+    </IfModule>
+    <IfModule mod_php.c>
+        <IfModule mod_mime.c>
+            AddType application/x-httpd-php .php
+        </IfModule>
+        <FilesMatch ".+\.php$">
+            SetHandler application/x-httpd-php
+        </FilesMatch>
+
+        php_value include_path .
+        php_admin_value upload_tmp_dir /var/lib/phpmyadmin/tmp
+        php_admin_value open_basedir /usr/share/phpmyadmin/:/etc/phpmyadmin/:/var/lib/phpmyadmin/:/usr/share/php/php-gettext/:/usr/share/php/php-php-gettext/:/usr/share/javascript/:/usr/share/php/tcpdf/:/usr/share/doc/phpmyadmin/:/usr/share/php/phpseclib/
+        php_admin_value mbstring.func_overload 0
+    </IfModule>
+
+</Directory>
+
+# Authorize for setup
+<Directory /usr/share/phpmyadmin/setup>
+    <IfModule mod_authz_core.c>
+        <IfModule mod_authn_file.c>
+            AuthType Basic
+            AuthName "phpMyAdmin Setup"
+            AuthUserFile /etc/phpmyadmin/htpasswd.setup
+        </IfModule>
+        Require valid-user
+    </IfModule>
+</Directory>
+
+# Disallow web access to directories that don't need it
+<Directory /usr/share/phpmyadmin/templates>
+    Require all denied
+</Directory>
+<Directory /usr/share/phpmyadmin/libraries>
+    Require all denied
+</Directory>
+<Directory /usr/share/phpmyadmin/setup/lib>
+    Require all denied
+</Directory>
+--------------------------------------------------------------------------
+
+You can restrict access from specific IP by adding line like below:
+
+        Require ip 127.0.0.1 192.168.18.0/24  // nem fontos
+--------------------------------------------------------------
+   link:     http://[ServerIP|Hostname]/phpmyadmin.
+   bejelentkezés mysql felhasználóval:
+   user:laravel_user pass:Password  // mariadb telepítésnél lett maegadva
